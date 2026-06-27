85862260a30da127c55af5d18445e749b9d69570
utsavanand131 <utsavanand807@gmail.com>
2026-06-27 20:48:33 +0530

    clean project: backend + frontend structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">Tailwind is Working 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
